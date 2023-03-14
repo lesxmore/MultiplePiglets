538134af77d12f33f9105c01b58c117a996731e7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-14 21:06:03 +0800

    Initial commit

---
+++ src/main/java/me/lesxmore/multiplepiglets/MultiplePiglets.java
@@ -0,0 +1,17 @@
+package me.lesxmore.multiplepiglets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MultiplePiglets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
