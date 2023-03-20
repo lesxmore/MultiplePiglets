7c9d367b5bbbe0e65862cf289472d09ca990a141
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 19:59:16 +0800

    MultiplePiglets 1.2 Added permission multiplepiglets.pigletsPerLitter.<maxpigletsPerLitter>

--- src/main/java/me/lesxmore/multiplepiglets/MultiplePiglets.java
+++ src/main/java/me/lesxmore/multiplepiglets/MultiplePiglets.java
@@ -1,5 +1,8 @@
 package me.lesxmore.multiplepiglets;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.EntityType;
@@ -11,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Random;
 
 public final class MultiplePiglets extends JavaPlugin implements Listener {
@@ -57,14 +61,50 @@ public final class MultiplePiglets extends JavaPlugin implements Listener {
         // Check if the bred entity is a pig
         if (event.getEntity().getType() == EntityType.PIG) {
             Pig parent = (Pig) event.getEntity();
-            // Set the number of piglets based on the pigletsPerLitter setting
+
+            // Get the maximum pigletsPerLitter allowed for the player's permission level
+            int maxPigletsPerLitter = 0;
+            for (int i = 10; i > 0; i--) {
+                String permission = "multiplepiglets.pigletsPerLitter." + i;
+                if (Objects.requireNonNull(event.getBreeder()).hasPermission(permission)) {
+                    maxPigletsPerLitter = i;
+                    break;
+                }
+            }
+
+            // If the player does not have any permission for pigletsPerLitter, cancel the event
+            if (maxPigletsPerLitter == 0) {
+                event.setCancelled(true);
+                return;
+            }
+
+            // Set the number of piglets based on the maximum pigletsPerLitter allowed
             Random rand = new Random();
-            int numPiglets = rand.nextInt(pigletsPerLitter - 1) + 2; // 2 to pigletsPerLitter piglets
+            int numPiglets = rand.nextInt(maxPigletsPerLitter - 1) + 2; // 2 to maxPigletsPerLitter piglets
+
             // Spawn the piglets
             for (int i = 0; i < numPiglets; i++) {
                 Pig piglet = (Pig) parent.getWorld().spawnEntity(parent.getLocation(), EntityType.PIG);
                 piglet.setBaby();
             }
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("multiplepigletsreload")) {
+            if (!sender.isOp()) {
+                sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                return true;
+            }
+
+            // Reload the configuration file
+            reloadConfig();
+            pigletsPerLitter = getConfig().getInt("pigletsPerLitter", pigletsPerLitter);
+            sender.sendMessage(ChatColor.GREEN + "Configuration file reloaded.");
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED + "Invalid command. Usage: /multiplepigletsreload");
+        return true;
     }
 }
